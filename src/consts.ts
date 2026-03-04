2092c55aab1ad9a016ed5d887d0fd1f755411699
nosocmasiu <masiuu007@gmail.com>
2026-03-04 13:57:41 +0100

    Web DMH Sales lista para publicar

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'DMH Sales';
+export const SITE_DESCRIPTION = 'Análisis por expertos, guías de compra y las mejores ofertas de Amazon España.';
